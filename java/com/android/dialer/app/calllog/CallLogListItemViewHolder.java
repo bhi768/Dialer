LineageOS/android_packages_apps_Dialer

Allow per-call account selection.
Change-Id: Ieac27294f4069b2cff17bb39a0954f74320ad943

java/com/android/dialer/app/calllog/CallLogListItemViewHolder.java
@@ -103,6 +103,7 @@ import java.lang.annotation.RetentionPolicy;
  */
 public final class CallLogListItemViewHolder extends RecyclerView.ViewHolder
     implements View.OnClickListener,
+        View.OnLongClickListener,
         MenuItem.OnMenuItemClickListener,
         View.OnCreateContextMenuListener {
   /** The root view of the call log list item */
@@ -300,6 +301,7 @@ public final class CallLogListItemViewHolder extends RecyclerView.ViewHolder
       quickContactView.setPrioritizedMimeType(Phone.CONTENT_ITEM_TYPE);
     }
     primaryActionButtonView.setOnClickListener(this);
+    primaryActionButtonView.setOnLongClickListener(this);
     primaryActionView.setOnClickListener(mExpandCollapseListener);
     if (mVoicemailPlaybackPresenter != null
         && ConfigProviderBindings.get(mContext)
@@ -504,6 +506,7 @@ public final class CallLogListItemViewHolder extends RecyclerView.ViewHolder
         primaryActionButtonView.setContentDescription(
             TextUtils.expandTemplate(
                 mContext.getString(R.string.description_voicemail_action), validNameOrNumber));
+        primaryActionButtonView.setTag(null);
         primaryActionButtonView.setVisibility(View.VISIBLE);
       } else {
         primaryActionButtonView.setVisibility(View.GONE);
@@ -880,7 +883,7 @@ public final class CallLogListItemViewHolder extends RecyclerView.ViewHolder
         return;
       }
 
-      final Intent intent = intentProvider.getIntent(mContext);
+      final Intent intent = intentProvider.getClickIntent(mContext);
       // See IntentProvider.getCallDetailIntentProvider() for why this may be null.
       if (intent == null) {
         return;
@@ -907,6 +910,18 @@ public final class CallLogListItemViewHolder extends RecyclerView.ViewHolder
         DialerUtils.startActivityWithErrorToast(mContext, intent);
       }
     }
+  }
+
+  @Override
+  public boolean onLongClick(View view) {
+    final IntentProvider intentProvider = (IntentProvider) view.getTag();
+    final Intent intent = intentProvider != null
+        ? intentProvider.getLongClickIntent(mContext) : null;
+    if (intent != null) {
+      DialerUtils.startActivityWithErrorToast(mContext, intent);
+      return true;
+    }
+    return false;
   }
 
   private void startLightbringerActivity(Intent intent) {

bhi768/Dialer

Signed-off-by: DennySPB <dennyspb@gmail.com>

java/com/android/dialer/app/calllog/CallLogListItemViewHolder.java
@@ -111,6 +111,7 @@ import java.lang.ref.WeakReference;
  */
 public final class CallLogListItemViewHolder extends RecyclerView.ViewHolder
     implements View.OnClickListener,
+        View.OnLongClickListener,
         MenuItem.OnMenuItemClickListener,
         View.OnCreateContextMenuListener {
 
@@ -313,6 +314,7 @@ public final class CallLogListItemViewHolder extends RecyclerView.ViewHolder
       quickContactView.setPrioritizedMimeType(Phone.CONTENT_ITEM_TYPE);
     }
     primaryActionButtonView.setOnClickListener(this);
+    primaryActionButtonView.setOnLongClickListener(this);
     primaryActionView.setOnClickListener(this.expandCollapseListener);
     if (this.voicemailPlaybackPresenter != null
         && ConfigProviderBindings.get(this.context)
@@ -525,6 +527,7 @@ public final class CallLogListItemViewHolder extends RecyclerView.ViewHolder
         primaryActionButtonView.setContentDescription(
             TextUtils.expandTemplate(
                 context.getString(R.string.description_voicemail_action), validNameOrNumber));
+        primaryActionButtonView.setTag(null);
         primaryActionButtonView.setVisibility(View.VISIBLE);
       } else {
         primaryActionButtonView.setVisibility(View.GONE);
@@ -1026,7 +1029,7 @@ public final class CallLogListItemViewHolder extends RecyclerView.ViewHolder
       return;
     }
 
-    final Intent intent = intentProvider.getIntent(context);
+    final Intent intent = intentProvider.getClickIntent(context);
     // See IntentProvider.getCallDetailIntentProvider() for why this may be null.
     if (intent == null) {
       return;
@@ -1053,6 +1056,18 @@ public final class CallLogListItemViewHolder extends RecyclerView.ViewHolder
 
       DialerUtils.startActivityWithErrorToast(context, intent);
     }
+  }
+
+  @Override
+  public boolean onLongClick(View view) {
+    final IntentProvider intentProvider = (IntentProvider) view.getTag();
+    final Intent intent = intentProvider != null
+        ? intentProvider.getLongClickIntent(context) : null;
+    if (intent != null) {
+      DialerUtils.startActivityWithErrorToast(context, intent);
+      return true;
+    }
+    return false;
   }
 
   private static boolean isNonContactEntry(ContactInfo info) {